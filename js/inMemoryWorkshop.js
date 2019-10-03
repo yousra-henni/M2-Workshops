a1c86e9cfb8da2d6411c3a2909b6f56578404ba7
jleveau <jleveau@laptop-147-210-129-134.labri.fr>
2019-10-03 15:03:38 +0200

    first commit

---
+++ js/inMemoryWorkshop.js
@@ -0,0 +1,53 @@
+inMemoryWorkshop = []
+
+
+function getWorkshopList() {
+    return new Promise((resolve, ) => {
+        resolve(inMemoryWorkshop)
+    })
+}
+
+function getWorkshopByName(name) {
+    return new Promise((resolve, reject) => {
+        if (!name) {
+            reject(new Error("name parameter is required"))
+        }
+        resolve(inMemoryWorkshop.find(workshop => workshop.name === workshop))
+    })
+}
+
+function addWorkshop(name, description) {
+    return new Promise((resolve, reject) => {
+        if (!name) {
+            reject(new Error("Workshop name required"))
+        }
+        if (!description) {
+            reject(new Error("Workshop description required"))
+        }
+        inMemoryWorkshop.push({
+            name,
+            description
+        })
+        resolve()
+    })
+}
+
+function removeWorkshopByName(name) {
+    return new Promise((resolve, reject) => {
+        reject(new Error("Not implemented"))
+    })
+}
+
+function updateWorkshop(name) {
+    return new Promise((resolve, reject) => {
+        reject(new Error("Not implemented"))
+    })
+}
+
+module.exports = {
+    getWorkshopList,
+    getWorkshopByName,
+    addWorkshop,
+    removeWorkshopByName,
+    updateWorkshop
+}
